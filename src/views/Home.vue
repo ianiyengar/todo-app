c6746409e9138ab1a52950495f95785761e7edea
Ian <1984therocker@gmail.com>
2021-08-04 17:03:02 -0500

    Index and Create functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>New To Do Item</h1>
+    <div>
+      Name:
+      <input type="text" v-model="newTodo.name" />
+      Item:
+      <input type="text" v-model="newTodo.item" />
+
+      <button v-on:click="createTodo()">Create To Do</button>
+    </div>
+    <h1>All To do's</h1>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <h2>{{ todo.name }}</h2>
+      <p>Name: {{ todo.name }}</p>
+      <p>Item: {{ todo.item }}</p>
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      todos: [],
+      newTodo: {},
+    };
+  },
+  created: function () {
+    this.indexTodos();
+  },
+  methods: {
+    indexTodos: function () {
+      axios.get("/todos").then((response) => {
+        console.log("todos index", response);
+        this.todos = response.data;
+      });
+    },
+    createTodo: function () {
+      var params = {
+        name: this.newTodo.name,
+        item: this.newTodo.item,
+      };
+      axios
+        .post("/todos", params)
+        .then((response) => {
+          console.log("todos create", response);
+          this.todos.push(response.data);
+          this.newTodo = {};
+        })
+        .catch((error) => {
+          console.log("todos create error", error.response);
+        });
+    },
   },
 };
 </script>
